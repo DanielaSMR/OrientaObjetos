99c8039824fcba32889d1799d57e94ab45a087b6
DanielaSMR <10828775@ieselgrao.org>
2023-12-13 23:09:47 +0100

    g

--- EA3/src/Parincipal.java
+++ EA3/src/Parincipal.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Principal {
+public class Parincipal {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Escribe la longitud de la contraseña");
--- EA3/src/Password.java
+++ EA3/src/Password.java
@@ -1,6 +1,6 @@
 public class Password {
     private int longitud = 8;
-    static String contrasenya;
+    static String contrasenya;//??
 
     public Password(){
 
@@ -15,15 +15,15 @@ public class Password {
         
     }
 
-    public String generarPassword(){
+    /*public String generarPassword(){
         
-        for(int i = 0; i >= longitud;i++){
-            contrasenya += (Math.random()*9 + 'a' + 'A' + 0);
+        for(int i = 0; i <= longitud;i++){
+            contrasenya += (Math.random()*9 + 'a');
         }
 
         return contrasenya;
 
-    }
+    }*/
 
     public int getLogintud() {
         return longitud;
